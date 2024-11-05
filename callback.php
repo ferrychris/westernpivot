d1d83c99335f24b6916d2edda67f7bd416df95f0
ferrychris <ferrychris95@gmail.com>
2024-11-05 14:48:28 +0100

    First release of Hello World!

---
+++ callback.php
@@ -0,0 +1,29 @@
+<?php
+require 'vendor/autoload.php';
+
+use Google\Auth\OAuth2;
+
+$oauth2 = new OAuth2([
+    'clientId' => '4680241477-m9pguejepqqevdq85mtfvtm1951k0pqt.apps.googleusercontent.com',
+    'clientSecret' => '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',
+    'authorizationUri' => 'https://accounts.google.com/o/oauth2/auth',
+    'redirectUri' => 'https://westernpivot.netlify.app/callback.php',
+    'tokenCredentialUri' => 'https://oauth2.googleapis.com/token',
+]);
+
+// Check if authorization code is in query parameters
+if (isset($_GET['code'])) {
+    $oauth2->setCode($_GET['code']);
+    
+    // Exchange authorization code for access token
+    $authToken = $oauth2->fetchAuthToken();
+    if (isset($authToken['access_token'])) {
+        // Store the access token and refresh token (if available) as needed
+        echo "Authentication successful!";
+    } else {
+        echo "Failed to obtain access token.";
+    }
+} else {
+    echo "No authorization code returned.";
+}
+?>
